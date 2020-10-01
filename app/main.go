0f48600f25dfcc2b536f87f97464c7cbd91a4435
Dr. Sebastian Mahr <s.mahr@sap.com>
2020-10-01 11:04:04 +0200

    inital code for credit conversion

--- app/main.go
+++ app/main.go
@@ -4,24 +4,56 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 type CalculationResult struct {
 	Result float64
 }
 
-func Calculate(w http.ResponseWriter, r *http.Request) {
-	calculationResult := CalculationResult{Result: 1234.0}
-
-	json.NewEncoder(w).Encode(calculationResult)
+type CalculationError struct {
+	Result string
 }
 
-func homePage(w http.ResponseWriter, r *http.Request) {
+func caculateDonationCredit(usd float64) float64 {
+	if usd == 0 {
+		return 0
+	}
+	if usd < 10 {
+		return 0.1
+	} else if usd < 100 {
+		return 0.15
+	} else if usd < 1000 {
+		return 0.2
+	} else if usd < 10000 {
+		return 0.5
+	} else {
+		return 2
+	}
+}
+
+func Calculate(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("GET params were:", r.URL.Query())
+	salesAmountString := r.URL.Query().Get("salesAmount")
+
+	if salesAmount, err := strconv.ParseFloat(salesAmountString, 64); err == nil {
+		fmt.Println("bla")
+		fmt.Println("input variable is:", salesAmount)
+		donCredits := caculateDonationCredit(salesAmount)
+
+		calculationResult := CalculationResult{Result: donCredits}
+		json.NewEncoder(w).Encode(calculationResult)
+	}
+
+}
+
+func calculationHandler(w http.ResponseWriter, r *http.Request) {
 	Calculate(w, r)
 }
 
 func setupRoutes() {
-	http.HandleFunc("/", homePage)
+	http.HandleFunc("/", calculationHandler)
 }
 
 func main() {
