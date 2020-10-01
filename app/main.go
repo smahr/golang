3fed0850f399f26c79f5d3a63cdeb13a1017d641
Dr. Sebastian Mahr <s.mahr@sap.com>
2020-10-01 09:50:19 +0200

    struct for calculation result

--- app/main.go
+++ app/main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"encoding/json"
+	"fmt"
+	"net/http"
 )
 
+type CalculationResult struct {
+	Result float64
+}
+
+func Calculate(w http.ResponseWriter, r *http.Request) {
+	calculationResult := CalculationResult{Result: 1234.0}
+
+	json.NewEncoder(w).Encode(calculationResult)
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "My Awesome Go App")
+	Calculate(w, r)
 }
 
 func setupRoutes() {
-  http.HandleFunc("/", homePage)
+	http.HandleFunc("/", homePage)
 }
 
 func main() {
-  fmt.Println("Go Web App Started on Port 3000")
-  setupRoutes()
-  http.ListenAndServe(":3000", nil)
+	fmt.Println("Go Web App Started on Port 3000")
+	setupRoutes()
+	http.ListenAndServe(":3000", nil)
 }
